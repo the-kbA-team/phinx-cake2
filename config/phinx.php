19795e299096c8ccd5535d15b12e4ff684129add
stosef <16034383+stosef@users.noreply.github.com>
2024-02-26 15:30:55 +0100

    #14 phpstan check via github workflow
    * github workflow to check php7.4 compatibility #14
    
    * use php 7.4 for composer for phpstan #14
    
    * fix php_version for composer install #14
    
    * fix php_version for phpstan #14
    
    * tell phpstan to look in vendor #14
    
    * tell phpstan to use phpstan.neon #14
    
    * add phpstan.neon file to repository #14
    
    * check php8.0 compatibility with phpstan #14
    
    * #14 require php >=7.4 <=8.3
    
    * #14 require php >=7.4 <8.4
    
    * check php8.x compatibility with phpstan #14
    
    * #14 use spaces instead of tabs in phpstan.neon
    
    * #14 convert tabs to spaces and remove unneeded linebreaks in phinx.php
    
    * #14 use matrix for phpstan testing
    
    * #14 use matrix for phpstan testing
    
    * #14 more compact check if dataSource is string

--- config/phinx.php
+++ config/phinx.php
@@ -1,5 +1,4 @@
 <?php
-
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
@@ -33,7 +32,6 @@ if (!include CAKE_CORE_INCLUDE_PATH . DS . 'Cake' . DS . 'bootstrap.php') {
     trigger_error('CakePHP core could not be found. CakePHP core should be at "'.CAKE_CORE_INCLUDE_PATH.'"', E_USER_ERROR);
 }
 
-
 /**
  * Plugin handling
  */
@@ -60,14 +58,13 @@ $dataSource = 'default';
 $pluginConfig = sprintf('%1$s%2$sConfig%2$sphinx.php', $migrationRoot, DS);
 if (file_exists($pluginConfig)) {
     require_once $pluginConfig;
-    if (Configure::check('phinx.datasource')) {
+    if (Configure::check('phinx.datasource') && is_string(Configure::read('phinx.datasource'))) {
         $dataSource = Configure::read('phinx.datasource');
     }
 }
 
 App::uses('ConnectionManager', 'Model');
 $dbConfig = ConnectionManager::getDataSource($dataSource);
-
 
 return [
     'paths' => [
